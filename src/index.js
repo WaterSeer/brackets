5ac88129933bd01e4248d259ee6d5f88dd195480
Mike Kargin <41961514+WaterSeer@users.noreply.github.com>
2021-03-15 09:17:50 +0300

    Update index.js
    Implementing function check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let onlyBrackets = "";
+  function isBrakets(symbol) {
+      for (let subArr of bracketsConfig){
+        if (subArr[0] === symbol || subArr[1] === symbol)
+            return true;
+      }
+      return false;
+  }
+  function isPair(openedSymbol, closedSymbol) {
+      for (let subArr of bracketsConfig){
+        if(subArr[0] === openedSymbol)
+            if (subArr[1] === closedSymbol)
+                return true;
+      }
+      return false;
+  }
+  function  isOpenedSymbol(symbol){
+      for (let subArr of bracketsConfig){
+          if (subArr[0] === symbol)
+              return true;
+      }
+      return false;
+  }
+  stack.push(str[0]);
+  for(let i = 1; i < str.length; i++)
+  {
+        if (isPair(stack[stack.length - 1], str[i]))
+            stack.pop();
+        else
+            if(isOpenedSymbol(str[i]))
+                stack.push(str[i]);
+            else
+                return false;
+    }
+    if (stack.length != 0)
+        return false;
+    else
+        return true;
 }
